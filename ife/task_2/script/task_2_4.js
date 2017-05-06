e7756beb402fa669071779b5bab410968886aa0c
zq <405967372@qq.com>
2017-05-06 22:49:16 +0800

    JavaScript

--- ife/task_2/script/task_2_4.js
+++ ife/task_2/script/task_2_4.js
@@ -68,7 +68,6 @@ function init() {
 
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
     var add_btn = document.getElementById("add-btn");
-    // add_btn.addEventListener("click",addBtnHandle(),false);
     add_btn.addEventListener("click",addBtnHandle);
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
     
